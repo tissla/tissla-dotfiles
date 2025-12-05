b85a03c89373776020b317c86739deef449608f0
Tissla <tissla@proton.me>
2025-12-05 09:50:53 +0100

    autoclose neotree

--- nvim/init.lua
+++ nvim/init.lua
@@ -436,16 +436,26 @@ require("lazy").setup({
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
-		cmd = "Neotree",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
+			"mason-org/mason.nvim",
 		},
-		keys = {
-			{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Explorer NeoTree" },
-			{ "<leader>fe", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
-		},
+		config = function()
+			require("neo-tree").setup({
+				window = { width = 32 },
+				close_if_last_window = true,
+				filesystem = {
+					filtered_items = {
+						hide_dotfiles = false,
+						hide_gitignored = false,
+					},
+					follow_current_file = { enabled = true },
+				},
+			})
+			vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+		end,
 	},
 })
 
