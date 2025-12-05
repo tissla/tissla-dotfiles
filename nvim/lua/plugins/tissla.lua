f68101260f4dd628544123184bc8c433085c4b8f
Tissla <tissla@proton.me>
2025-12-05 07:19:12 +0100

    Lazyvim setup

--- nvim/lua/plugins/tissla.lua
+++ nvim/lua/plugins/tissla.lua
@@ -18,6 +18,36 @@ return {
     },
   },
 
+  -- noice
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        cmdline = {
+          enabled = true,
+          view = "cmdline_popup",
+        },
+        messages = { enabled = true },
+        presets = {
+          command_palette = true,
+        },
+      })
+
+      vim.api.nvim_set_hl(0, "NoiceCmdlinePrompt", { bg = "#1e1b29", fg = "#9ca3af" })
+
+      vim.api.nvim_set_hl(0, "NoiceCmdlineIcon", { fg = "#ff9e64" })
+
+      vim.api.nvim_set_hl(0, "NoiceCmdlinePopup", { bg = "#1e1b29", fg = "#c9c7d4" })
+      vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder", { fg = "#8b5cf6" })
+      vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderSearch", { fg = "#8b5cf6" })
+      vim.api.nvim_set_hl(0, "NoiceCmdlinePopupTitle", { fg = "#f9e2af", bold = true })
+    end,
+  },
+
   -- neotree
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -26,29 +56,25 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
+      "mason-org/mason.nvim",
     },
-
     config = function()
       require("neo-tree").setup({
-        window = {
-          width = 32,
-        },
+        window = { width = 32 },
         close_if_last_window = true,
         filesystem = {
           filtered_items = {
             hide_dotfiles = false,
             hide_gitignored = false,
           },
-          follow_current_file = {
-            enabled = true,
-          },
+          follow_current_file = { enabled = true },
         },
       })
       vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
     end,
   },
 
-  -- theme
+  -- colorscheme
   {
     "gmr458/vscode_modern_theme.nvim",
     lazy = false,
@@ -60,77 +86,100 @@ return {
         transparent_background = true,
       })
       vim.cmd.colorscheme("vscode_modern")
-
-      -- normal line numbering
       vim.opt.number = true
       vim.opt.relativenumber = false
-
-      -- transparent background
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-
-      -- cursorline clr
       vim.api.nvim_set_hl(0, "CursorLine", { bg = "#41395e" })
-
-      -- vim.api.nvim_set_hl(0, "Comment", { fg = "#6a9955", italic = true, bold = false })
       vim.api.nvim_set_hl(0, "LineNr", { fg = "#6b7280", bg = "none" })
       vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ff9e64", bg = "none", bold = true })
-
       vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-
-      -- neotree if used
       vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
       vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
       vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#ff9e64" })
-
-      -- Snacks side explorer tree
       vim.api.nvim_set_hl(0, "SnacksPickerList", { bg = "none", default = false })
-
       vim.api.nvim_set_hl(0, "SnacksPickerTree", { bg = "none" })
-
       vim.api.nvim_set_hl(0, "SnacksBackdrop", { bg = "none" })
-
-      -- top of tree
       vim.api.nvim_set_hl(0, "SnacksPickerInput", { bg = "none" })
-
       vim.api.nvim_set_hl(0, "SnacksPickerPrompt", { bg = "none" })
-
       vim.api.nvim_set_hl(0, "SnacksPickerInputBorder", { bg = "none" })
-      -- background var
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   },
 
-  -- installs
+  -- mason autosetup
+  "mason-org/mason-lspconfig.nvim",
+  opts = {},
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+
+  -- lspconfig
   {
-    "LazyVim/LazyVim",
+    "neovim/nvim-lspconfig",
     opts = {
-      -- colorscheme = function()
-      --   vim.cmd([[colorscheme tokyonight]])
-      --   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      -- end,
-      mason = {
-        ensure_installed = {
-          "stylua",
-          "shellcheck",
-          "shfmt",
-          "flake8",
-
-          -- added
-          "qmlformat",
-          "prettier",
-          "rustfmt",
-          "clang-format",
-          "goimports",
-          "gofmt",
-
-          --lsp
-          "qmlls",
-          "rust_analyzer",
-          "lua_ls",
-          "clangd",
+      servers = {
+        clangd = {
+          cmd = { "clangd", "--background-index", "--clang-tidy", "--completion-style=detailed" },
+          init_options = {
+            clangdFileStatus = true,
+            usePlaceholders = true,
+          },
+        },
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = { allFeatures = true },
+              procMacro = { enable = true },
+              checkOnSave = { command = "clippy" },
+            },
+          },
+        },
+        qmlls = {
+          cmd = { "qmlls6", "-E" },
+          cmd_env = {
+            qmlls_build_dirs = "/usr/lib/qt6/qml",
+            qml_import_path = table.concat({
+              "/usr/lib/qt6/qml",
+              vim.fn.expand("~/.config/quickshell"),
+            }, ":"),
+            qml2_import_path = "/usr/lib/qt6/qml",
+          },
         },
       },
     },
+  },
+
+  -- conform formatting
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        rust = { "rustfmt" },
+        go = { "goimports", "gofmt" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        qml = { "qmlformat" },
+      },
+    },
+  },
+
+  -- go setup
+  {
+    "ray-x/go.nvim",
+    dependencies = { "ray-x/guihua.lua" },
+    config = function()
+      require("go").setup()
+    end,
+    ft = { "go" },
   },
 }
