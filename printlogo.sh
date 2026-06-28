e5e28c7cd686a6e31b7083e516f45cc8bae8eac1
Tissla <tissla@proton.me>
2026-06-28 04:09:08 +0200

    printlogo.sh missing end quote

--- printlogo.sh
+++ printlogo.sh
@@ -2,7 +2,7 @@
 
 centerfile() {
     local term_width=$(tput cols)
-    
+
     # find longest row
     local max_length=0
     while IFS= read -r line; do
@@ -10,14 +10,14 @@ centerfile() {
         if [ ${#plain} -gt $max_length ]; then
             max_length=${#plain}
         fi
-    done < "$1"
-    
+    done <"$1"
+
     # center based on longest row
-    local padding=$(( (term_width - max_length) / 2 ))
-    
+    local padding=$(((term_width - max_length) / 2))
+
     while IFS= read -r line; do
         printf "%${padding}s%s\n" "" "$line"
-    done < "$1"
+    done <"$1"
 }
 
-centerfile "$(dirname "$0")/fastfetch/tissla.txt
+centerfile "$(dirname "$0")/fastfetch/tissla.txt"
