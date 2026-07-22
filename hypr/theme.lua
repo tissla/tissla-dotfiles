4508150ac3d248ed9e7b7bb722c4700c929fa56d
Tissla <tissla@proton.me>
2026-07-22 23:50:53 +0200

    fix: matugen

--- hypr/theme.lua
+++ hypr/theme.lua
@@ -1,10 +1,10 @@
 local colors = {
-    base       = "rgb(101417)",
-    text       = "rgb(e0e3e8)",
-    accent     = "rgb(97ccf8)",
-    surface1   = "rgb(262a2e)",
-    bgalpha    = "0xee101417",
-    mutedalpha = "0xaa262a2e",
+    base       = "rgb(12140e)",
+    text       = "rgb(e3e3d8)",
+    accent     = "rgb(b4d087)",
+    surface1   = "rgb(292b23)",
+    bgalpha    = "0xee12140e",
+    mutedalpha = "0xaa292b23",
 }
 
 return colors
